ff16c7da325df2eddac8e9a310164d4e910d260d
dhirajraut1 <rautdhiraj277@gmail.com>
2024-06-28 21:49:15 +0545

    fix port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs-extra');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
